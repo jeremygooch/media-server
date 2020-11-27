62dace60b65a12f12454f645fbec3a8b62e19661
Jeremy Gooch <jeremy.gooch@gmail.com>
2020-11-27 15:34:56 -0600

    Implementing simple auth module with jwt and passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VideoController } from 'controllers';
 import { VideoService } from 'services';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from 'app.controller';
 
 @Module({
-  imports: [],
-  controllers: [VideoController],
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController, VideoController],
   providers: [VideoService],
 })
 export class AppModule {}
