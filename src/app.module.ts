c9f58ba8797177ae3d2b8319160d2ec22bff486c
Jeremy Gooch <jeremy.gooch@gmail.com>
2020-11-28 14:49:27 -0600

    Updating docker compose to use env vars. Also adding user entity and typeorm to nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from 'app.controller';
 import { PlaybackModule } from './playback/playback.module';
+import { Connection } from 'typeorm';
+import { User } from 'users/user.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule, PlaybackModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'media',
+      password: 'bDlLabh766ddw4c',
+      database: 'media-server',
+      entities: [User],
+      synchronize: true // DEBUGGING ONLY
+    }),
+    AuthModule,
+    UsersModule,
+    PlaybackModule
+  ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
